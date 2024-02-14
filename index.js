bc4a93f74494044149112274dee6bda758437c98
BHAVANISHANKARAN <bhavanishankaran2003@gmail.com>
2024-02-15 02:28:29 +0530

    after installing morgan

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import path from "path";
 import usersService from "./services/users.service.js";
 import { UserToken } from "./models/userToken.model.js";
 import cors from "cors";
+import morgan from "morgan";
 
 // app.post("")
 // const sequelize = new Sequelize(
@@ -39,7 +40,7 @@ try {
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.use(morgan("dev"));
 app.use("/music", musicRouter);
 app.use("/users", userRouter);
 
